60da4f8435256476eb302994d5cd54286dddaa20
IInventedDirt <olorin_67@yahoo.com>
2022-09-19 00:34:19 -0500

    Revert "View zooms in when using the gun just like the Bow."
    This reverts commit 59cf3a7a3c51135bb7e55831a24e8bd248639563.

--- src/main/java/net/dirtengineers/squirtgun/client/events/ClientForgeEventHandler.java
+++ src/main/java/net/dirtengineers/squirtgun/client/events/ClientForgeEventHandler.java
@@ -1,32 +1,43 @@
 package net.dirtengineers.squirtgun.client.events;
 
 import net.dirtengineers.squirtgun.Squirtgun;
-import net.dirtengineers.squirtgun.common.item.SquirtgunItem;
+import net.dirtengineers.squirtgun.common.registry.ItemRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ComputeFovModifierEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = Squirtgun.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ClientForgeEventHandler {
     @SubscribeEvent
-    public static void onComputeFovModifierEvent(ComputeFovModifierEvent event) {
-        if (event.getPlayer().isUsingItem() && event.getPlayer().getUseItem().getItem() instanceof SquirtgunItem) {
-            float f = 1f;
-            int ticksUsingItem = event.getPlayer().getTicksUsingItem();
-            if(ticksUsingItem >= 15){
-                int x = 1;
-            }
-            float f1 = (float) ticksUsingItem / 20.0F;
-            if (f1 > 1.0F) {
-                f1 = 1.0F;
-            } else {
-                f1 *= f1;
-            }
+    public static float onComputeFovModifierEvent(ComputeFovModifierEvent event) {
+        float f = 1F;
+        Player player = event.getPlayer();
+        ItemStack itemstack = player.getUseItem();
+        if (player.isUsingItem()) {
+            if (itemstack.is(ItemRegistration.SQUIRTGUN.get())) {
+                int i = player.getTicksUsingItem();
+                if(i >= 15){
+                    int x = 1;
+                }
+                float f1 = (float) i / 20.0F;
+                if (f1 > 1.0F) {
+                    f1 = 1.0F;
+                } else {
+                    f1 *= f1;
+                }
 
-            f *= 1.0F - f1 * 0.15F;
-
-            event.setNewFovModifier(f);
+                f *= 1.0F - f1 * 0.15F;
+            } else if (Minecraft.getInstance().options.getCameraType().isFirstPerson() && player.isScoping()) {
+                return 0.1F;
+            }
         }
+        event = new ComputeFovModifierEvent(event.getPlayer(), f);
+        MinecraftForge.EVENT_BUS.post(event);
+        return event.getNewFovModifier();//net.minecraftforge.client.ForgeHooksClient.getFieldOfViewModifier(player, f);
     }
 }
