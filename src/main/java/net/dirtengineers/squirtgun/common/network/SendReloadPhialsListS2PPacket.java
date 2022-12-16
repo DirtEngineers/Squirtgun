f0aa969bfc98fef7b02df204cc17be21c5382f0e
X_Niter <drumsforlife1999@yahoo.com>
2022-12-16 04:15:26 -0600

    Functioning sendReload packet

--- src/main/java/net/dirtengineers/squirtgun/common/network/SendReloadPhialsListS2PPacket.java
+++ src/main/java/net/dirtengineers/squirtgun/common/network/SendReloadPhialsListS2PPacket.java
@@ -1,24 +1,40 @@
 package net.dirtengineers.squirtgun.common.network;
 
+import net.dirtengineers.squirtgun.util.ReloadScreenHelper;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.network.NetworkEvent;
 
+import java.util.LinkedList;
 import java.util.function.Supplier;
 
-public class SendReloadPhialsListS2PPacket {
+public class SendReloadPhialsListS2PPacket extends ReloadScreenHelper {
 
-    public SendReloadPhialsListS2PPacket() {}
+    private final LinkedList<ItemStack> packetPhials;
 
-    public SendReloadPhialsListS2PPacket(FriendlyByteBuf pBuffer) {}
+    public SendReloadPhialsListS2PPacket(LinkedList<ItemStack> phials) {
+        this.packetPhials = phials;
+    }
+
+    public SendReloadPhialsListS2PPacket(FriendlyByteBuf pBuffer) {
+        int size = pBuffer.readInt();
+        this.packetPhials = new LinkedList<>();
+        for (int i = 0; i < size; i++) {
+            this.packetPhials.add(pBuffer.readItem());
+        }
+    }
 
     public void encode(FriendlyByteBuf pBuffer) {
-//        pBuffer.writeItemStack(ItemStack.EMPTY, false);
+        pBuffer.writeInt(packetPhials.size());
+        for (ItemStack phial : packetPhials) {
+            pBuffer.writeItemStack(phial, false);
+        }
     }
 
     public void handle(Supplier<NetworkEvent.Context> pContext) {
         pContext.get().enqueueWork(() -> {
-//            DistExecutor.unsafeRunWhenOn(Dist.CLIENT,
-//                    () -> () -> SquirtgunReloadScreen.updateTest(true));
+            // Update the list of phials for SquirtgunReloadScreen
+            ReloadScreenHelper.phials = this.packetPhials;
         });
 
         pContext.get().setPacketHandled(true);
